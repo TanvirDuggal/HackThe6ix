ea7dabc89e26c6f034f5e55c9b6e5e31c0ed628d
Tanvy <dggaltanvir@gmail.com>
2021-08-16 22:28:29 -0400

    Basic stuffs

---
+++ 1. Basic/Basic_1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 22:24:56 2021
+
+@author: Tanvy
+"""
+
+# Python 101
+# In this tutorial you will learn how to do some basic stuffs in python.
+
+# 1. The old classic print 'Hello World'
+print("Hello World!!!")
+
+# 2. Declaring variables
+a = "Hello World!!!"
+print(a)
+
+# 3. Lets work with numbers OLD way
+
+num1 = 5
+num2 = 8
+
+print("Addition")
+print(num1+num2)
+print("Substraction")
+print(num1-num2)
+print("Division")
+print(num1/num2)
+print("Multipications")
+print(num1*num2)
+
+
+# 4. Loops; LIFE LINE
+
+for i in range(10):
+    print(i)
+    
+# 5. IFs - Becasue I NEED THEM
+
+x = 5
+
+if x == 5:
+    print("Yes")
+else:
+    print("No")
+    
